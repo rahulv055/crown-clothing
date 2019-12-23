b976bf9edf4b10b48bc09a3160114f3f9279eba3
Rahul Vernekar <rahulprakashvernekar@yahoo.com>
2019-12-23 20:00:42 +0530

    implemented storing of login details to firestore and app

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,7 +14,32 @@ const config = {
   measurementId: "G-KHJVF12YE1"
 };
 
+export const createUserProfileDocument = async (userAuth,additionalData) => {
+  if(!userAuth) return;
 
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapshot = await userRef.get();
+
+  if(!snapshot.exists){
+    const {displayName,email} = userAuth;
+    const creatAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        creatAt,
+        ...additionalData
+      })
+    }catch(error){
+      console.log("error creating user",error);
+    }
+
+  }
+
+  return userRef;
+}
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();
