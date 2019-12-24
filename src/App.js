32f7c3a5f75584e0c7529884df57b95e590df1e7
Rahul Vernekar <rahulprakashvernekar@yahoo.com>
2019-12-24 22:50:28 +0530

    implemented redirect to home on login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route,Switch } from "react-router-dom";
+import { Route,Switch,Redirect } from "react-router-dom";
 import {connect} from 'react-redux';
 import HomePage from './pages/homepage.components';
 import ShopPage from "./pages/shop/shop.components";
@@ -37,16 +37,26 @@ class App extends React.Component {
         <Header/>
         <Switch>
         <Route exact path="/" component={HomePage}/>   
-        <Route path="/shop" component={ShopPage}/>  
-        <Route path="/signin" component={SignInAndSignUpPage}/>  
+        <Route exact path="/shop" component={ShopPage}/>  
+         <Route  
+         exact 
+         path="/signin" render={
+          () =>
+        this.props.currentUser ?
+        (<Redirect to='/' />)
+         :
+         (<SignInAndSignUpPage/>)
+       }/>  
         </Switch>
      </div>
     );
   }
   
 }
-
+const mapStateToProps = ({user}) => ({
+    currentUser:user.currentUser
+})
 const mapDispatchToProps = dispatch => ({
        setCurrentUser: user => dispatch(setCurrentUser(user))
 })
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
