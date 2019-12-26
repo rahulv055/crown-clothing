0c6d1e04fb32223fee8c37357429b483e18aba6d
Rahul Vernekar <rahulprakashvernekar@yahoo.com>
2019-12-26 23:41:21 +0530

    add to cart implemented and count o fitem added to cart

--- src/components/cart-dropdown/cart-dropdown.components.jsx
+++ src/components/cart-dropdown/cart-dropdown.components.jsx
@@ -1,14 +1,27 @@
 import React from "react";
-
-import CustomButton from '../custom-button/custom-button.components'
+import { connect } from "react-redux";
+import CustomButton from '../custom-button/custom-button.components';
+import CartItem from '../../components/cart-item/cart-item.components'
 
 import './cart-dropdown.styles.scss'
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
+
     <div className="cart-dropdown">
-        <div className="cart-items" />
+        <div className="cart-items">
+            {
+                cartItems.map(cartItem =>
+                    (<CartItem key={cartItem.id} items={cartItem} />)
+                )
+            }
+        </div>
         <CustomButton>GO TO CHECKOUTS</CustomButton>
     </div>
+
 )
 
-export default CartDropdown;
+const mapStateToProps = (state) => ({
+    cartItems: state.cart.cartItems
+})
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/components/cart-icon/cart-icon.components.jsx
+++ src/components/cart-icon/cart-icon.components.jsx
@@ -2,18 +2,29 @@ import React from 'react';
 import { connect } from "react-redux";
 import { ReactComponent as ShoppingIcon } from "../../assests/shopping-bag.svg";
 import { toggleCartHidden } from "../../redux/cart-reducer/cart.actions";
-
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
+function countItems(cartItems) {
+    let count = 0;
+    cartItems.forEach(element => {
+        count = count + element.quantity;
+    });
+    return count;
+}
+
+const CartIcon = ({ toggleCartHidden, cartItems }) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className="shopping-icon" />
-        <span className="item-count">0</span>
+        <span className="item-count">{countItems(cartItems)}</span>
     </div>
 )
+
+const mapStateToProps = (state) => ({
+    cartItems: state.cart.cartItems
+})
 
 const mapsDipatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null, mapsDipatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapsDipatchToProps)(CartIcon);
