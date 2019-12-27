0f09cc629a3c2431b64722ef4dad8ef0717f80a3
Rahul Vernekar <rahulprakashvernekar@yahoo.com>
2019-12-27 12:42:59 +0530

    implemented count items in cart using reduce method

--- src/components/cart-icon/cart-icon.components.jsx
+++ src/components/cart-icon/cart-icon.components.jsx
@@ -4,23 +4,24 @@ import { ReactComponent as ShoppingIcon } from "../../assests/shopping-bag.svg";
 import { toggleCartHidden } from "../../redux/cart-reducer/cart.actions";
 import './cart-icon.styles.scss';
 
-function countItems(cartItems) {
-    let count = 0;
-    cartItems.forEach(element => {
-        count = count + element.quantity;
-    });
-    return count;
-}
+// function countItems(cartItems) {
+//     let count = 0;
+//     cartItems.forEach(element => {
+//         count = count + element.quantity;
+//     });
+//     return count;
+// }
 
-const CartIcon = ({ toggleCartHidden, cartItems }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className="shopping-icon" />
-        <span className="item-count">{countItems(cartItems)}</span>
+        <span className="item-count">{itemCount}</span>
     </div>
 )
 
-const mapStateToProps = (state) => ({
-    cartItems: state.cart.cartItems
+const mapStateToProps = ({cart:{cartItems}}) => ({
+    // cartItems: state.cart.cartItems
+    itemCount:cartItems.reduce((accumalatedQuatity,cartItem)=>accumalatedQuatity+cartItem.quantity,0)
 })
 
 const mapsDipatchToProps = dispatch => ({
