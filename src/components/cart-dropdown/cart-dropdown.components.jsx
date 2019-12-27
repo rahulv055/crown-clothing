5c3e90f85c45f8613b5fa5da706be2fdf094dd8b
Rahul Vernekar <rahulprakashvernekar@yahoo.com>
2019-12-27 23:30:19 +0530

    added empty message to cart

--- src/components/cart-dropdown/cart-dropdown.components.jsx
+++ src/components/cart-dropdown/cart-dropdown.components.jsx
@@ -10,9 +10,13 @@ const CartDropdown = ({ cartItems }) => (
     <div className="cart-dropdown">
         <div className="cart-items">
             {
-                cartItems.map(cartItem =>
-                    (<CartItem key={cartItem.id} items={cartItem} />)
-                )
+                cartItems.length ?
+                    (cartItems.map(cartItem =>
+                        (<CartItem key={cartItem.id} items={cartItem} />)
+                    ))
+                    : (
+                        <span className="empty-message">YOUR CART IS EMPTY</span>
+                    )
             }
         </div>
         <CustomButton>GO TO CHECKOUTS</CustomButton>
