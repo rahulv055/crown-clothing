3c83727cc137d1ce5d1cec7d743d30d7ca4c1c4c
Rahul Vernekar <rahulprakashvernekar@yahoo.com>
2019-12-28 20:11:50 +0530

    moved directory sections to redux

--- src/components/directory/directory.components.jsx
+++ src/components/directory/directory.components.jsx
@@ -5,16 +5,18 @@ import MenuItem from "../menu-item/menu-item.components";
 import { selectDirectorySections } from "../../redux/directory-reducer/directory.selectors";
 import "./directory.styles.scss";
 
-const Directory = ({ sections }) => (
-  <div className='directory-menu'>
-    {
-      sections.map(section => (
-        <MenuItem key={section.id} title={section.title} imageUrl={section.imageUrl} size={section.size} linkUrl={section.linkUrl} />
-      ))
-    }
-  </div>
+const Directory = ({ sections }) => {
+  return (
+    <div className='directory-menu'>
+      {
+        sections.map(section => (
+          <MenuItem key={section.id} title={section.title} imageUrl={section.imageUrl} size={section.size} linkUrl={section.linkUrl} />
+        ))
+      }
+    </div>
+  );
+}
 
-);
 
 const mapStateToProps = createStructuredSelector({
   sections: selectDirectorySections
